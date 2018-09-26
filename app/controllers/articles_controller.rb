2d3e45d16c5e7ad02a2eb02d6d9adf0badc61f96
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-09-26 19:35:25 +0900

    Add syusei

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,25 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+
   def show
   end
 
-  # GET /articles/new
+
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
+
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
+
   def create
     @article = Article.new(article_params)
 
